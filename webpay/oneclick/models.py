f529a4f786112c136fc4c84dec72ae3ceba77b2d
misalabs <arensiatik@gmail.com>
2019-01-25 21:08:48 +0000

    Adding modelo y guardado de info

---
+++ webpay/oneclick/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+class WebpayOneClickInscription(models.Model):
+    """
+    Modelo para guardar informacion de OneClick
+    """
+    token = models.CharField(
+        'Token proporcionado por Transbank', max_length=300, blank=True, unique=True)
+    user = models.CharField(
+        'Username del usuario del comercio', max_length=100, blank=True)
+    tbk_user = models.CharField(
+        'Username del usuario en Transbank', max_length=200, blank=True)
+    response_code = models.CharField(
+        'Código respuesta de la autorizacion', max_length=10, blank=True)
+    authorization_code = models.CharField(
+        'Código autorización de la transacción', max_length=8, blank=True)
+    creditcard_type = models.CharField(
+        'Tipo de tarjeta inscrita por el cliente', max_length=10, blank=True)
+    card_number = models.CharField(
+        'Últimos 4 números de la tarjeta', max_length=4, blank=True)
+    date_inscription = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        db_table = "webpay_oneclick_inscription"
+        verbose_name = "Webpay OneClick Inscription"
--- webpay/oneclick/views.py
+++ webpay/oneclick/views.py
@@ -3,11 +3,27 @@ import traceback
 
 from django.conf import settings
 from django.http import HttpResponse, HttpResponseBadRequest
+from django.template import Context, Template
 from django.views.decorators.csrf import csrf_exempt
 
 from .communication import WebpayOneClickWS
+from .models import WebpayOneClickInscription
 
 logger = logging.getLogger(__name__)
+
+
+def webpay_oneclick_model(token, get_finish_inscription):
+    """
+    Metodo que ayudara a guardar el modelo.
+    """
+    oneclick_model = WebpayOneClickInscription.objects.get(
+        token=token, response_code__isnull=True)
+    oneclick_model.tbk_user = get_finish_inscription.get('tbkUser')
+    oneclick_model.response_code = get_finish_inscription.get('responseCode')
+    oneclick_model.authorization_code = get_finish_inscription.get('authCode')
+    oneclick_model.card_number = get_finish_inscription.get('last4CardDigits')
+    oneclick_model.creditcard_type = get_finish_inscription.get('creditCardType')
+    oneclick_model.save()
 
 
 @csrf_exempt
@@ -25,6 +41,7 @@ def webpay_oneclick_finish(request):
             get_finish_inscription = WebpayOneClickWS().finishInscription(token)
             logger.debug('Webpay OneClick. Token {} Respuesta {}'.format(
                 token, get_finish_inscription))
+            webpay_oneclick_model(get_finish_inscription)
         except Exception, e:
             logger.error('Webpay OneClick. Ocurrion un error al consultar Token enviado por Transbank {}. Error {} Traza {}'.format(
                 token, e, traceback.format_exc()))
