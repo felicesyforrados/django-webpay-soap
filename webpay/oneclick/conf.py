0397fa0f89522fff1b257d65193389bfd96e8b62
misalabs <arensiatik@gmail.com>
2019-01-30 18:27:34 +0000

    Adding Authorization Payment model

--- webpay/oneclick/conf.py
+++ webpay/oneclick/conf.py
@@ -3,3 +3,18 @@ RESPONSE_CODES = {
     '0': 'Inscripción aprobada.',
     '-3': 'Inscripción rechazada.',
 }
+
+
+PAYMENT_RESPONSE_CODES = {
+    '0': 'Pago aprobado.',
+    '-1': 'Inscripción rechazada.',
+    '-2': 'Inscripción rechazada.',
+    '-3': 'Inscripción rechazada.',
+    '-4': 'Inscripción rechazada.',
+    '-5': 'Inscripción rechazada.',
+    '-6': 'Inscripción rechazada.',
+    '-7': 'Inscripción rechazada.',
+    '-8': 'Inscripción rechazada.',
+    '-98': 'Límites Oneclick, máximo monto de pago excedido',
+    '-99': 'Límites Oneclick, máxima cantidad de pagos diarios excedido.'
+}
--- webpay/oneclick/models.py
+++ webpay/oneclick/models.py
@@ -1,16 +1,17 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
-from .conf import RESPONSE_CODES
+from .conf import RESPONSE_CODES, PAYMENT_RESPONSE_CODES
 
 
 class WebpayOneClickInscription(models.Model):
     """
     Modelo para guardar informacion de OneClick
     """
+    user = models.CharField(
+        'Username del usuario del comercio', max_length=100, primary_key=True)
     token = models.CharField(
         'Token proporcionado por Transbank', max_length=300, blank=True)
-    user = models.CharField(
-        'Username del usuario del comercio', max_length=100, blank=True)
     tbk_user = models.CharField(
         'Username del usuario en Transbank', max_length=200, blank=True)
     response_code = models.CharField(
@@ -34,3 +35,34 @@ class WebpayOneClickInscription(models.Model):
     class Meta:
         db_table = "webpay_oneclick_inscription"
         verbose_name = "Webpay OneClick Inscription"
+
+
+class WebpayOneClickPayment(models.Model):
+    """
+    Modelo para guardar informacion de los pagos autorizados mediante OneClick.
+    """
+    user = models.ForeignKey(WebpayOneClickInscription, blank=False, null=False)
+    buy_order = models.CharField(
+        'Orden Compra de la tienda', max_length=42, unique=True)
+    amount = models.PositiveIntegerField('Monto transacción', default=0)
+    authorization_code = models.CharField(
+        'Código autorización de la transacción', max_length=8, blank=True)
+    credit_card_type = models.CharField(
+        'Tipo de tarjeta inscrita por el cliente', max_length=10, blank=True)
+    last4_card_digits = models.CharField(
+        'Últimos 4 números de la tarjeta', max_length=4, blank=True)
+    transaction_id = models.CharField(
+        'Transaction ID', max_length=4, blank=True)
+    response_code = models.CharField(
+        'Código respuesta de la autorizacion', max_length=10, blank=True)
+
+    @property
+    def humanized_response_code(self):
+        """
+        Response code humanizado
+        """
+        return PAYMENT_RESPONSE_CODES.get(self.response_code)
+
+    class Meta:
+        db_table = "webpay_oneclick_payment"
+        verbose_name = "Webpay OneClick Payment"
